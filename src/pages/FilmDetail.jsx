c8ed7823d3d8886c891467afe6d6a5cc370bcc51
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2026-02-23 15:40:52 +0100

    struttura FilmDetail

--- src/pages/FilmDetail.jsx
+++ src/pages/FilmDetail.jsx
@@ -5,7 +5,15 @@ const FilmDetail = () => {
     const { id } = useParams()
 
     return (
+        <>
         <h1>FilmDetail{id}</h1>
+        <div className="film-container">
+            <h3>title</h3>
+            <img src="" alt="" />
+            <p>genre</p>
+            <p>release year</p>
+        </div>
+        </>
     )
 }
 
