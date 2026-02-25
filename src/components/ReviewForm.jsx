8a637f81e322d6481a708faee4ae40662413cb47
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2026-02-25 14:21:01 +0100

    componente per il form

---
+++ src/components/ReviewForm.jsx
@@ -0,0 +1,15 @@
+// creo un form
+import { useState } from "react";
+
+// var di stato per salvare l'input dell'utente
+const [formData, setFormData] = useState([])
+
+// funzione per gestire i dati del form
+// nel nuovo oggetto dovranno esserci i dati precedenti + il nuovo valore
+
+// funzione per gestire chiamata e submit del form
+// submit per nn perdere i dati al refresh della pagina
+// fare chiamata con axios
+// fare in modo che mi svuota i campi del form dopo aver fatto la chiamata
+
+// return con il form e i dati da raccogliere
